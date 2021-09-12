93a42e29d07bcae3069aa742d396a041a2d3c6ec
ProjectMayuki <ProjectMayuki@gmail.com>
2021-09-12 22:30:50 +0900

    IServerProcessをインターフェースクラスに(メンバ持たない)

--- GameAI/Server/IServerProcess.py
+++ GameAI/Server/IServerProcess.py
@@ -9,17 +9,16 @@ class IServerProcess:
 
     ADDR = "127.0.0.1"
     
-    def __init__(self, port, opt = {"ServerMaxClientNum" : 5, "ServerBufferSize" : 4096}):
-        self._port = port
-        self._opt = opt
+    def __init__(self):
+        pass
     # __init__
 
-    def execute(self):
+    def execute(self, port, opt = {"ServerMaxClientNum" : 5, "ServerBufferSize" : 4096}):
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as _socket:
-            _socket.bind( (IServerProcess.ADDR, self._port) )
+            _socket.bind( (IServerProcess.ADDR, port) )
 
             while True:
-                recieve_raw_data, client_addr = _socket.recvfrom(self._opt["ServerBufferSize"])
+                recieve_raw_data, client_addr = _socket.recvfrom(opt["ServerBufferSize"])
                 receive_data = pickle.loads(recieve_raw_data)
 
                 if self._receive_admin_command(receive_data, client_addr, _socket):
@@ -55,5 +54,5 @@ class IServerProcess:
     # _receive_admin_command
 
 if __name__ == '__main__':
-    server = IServerProcess(7010)
-    server.execute()
+    server = IServerProcess()
+    server.execute(7010)
