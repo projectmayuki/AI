f6d52761050941ff223b5542c492fc124e308808
ProjectMayuki <ProjectMayuki@gmail.com>
2022-03-16 21:22:42 +0900

    テストスクリプトの共有コードまとめた

---
+++ GameAI/TestScripts/TestServerSetting.py
@@ -0,0 +1,24 @@
+
+
+class TestServerSetting:
+
+    def __init__(self):
+        pass
+    #
+
+    @classmethod
+    def addr(self):
+        return '127.0.0.1'
+    #
+
+    @classmethod
+    def port(self):
+        return 7010
+    #
+
+    @classmethod
+    def msg_buf_size(self):
+        return 4096
+    #
+#
+    
